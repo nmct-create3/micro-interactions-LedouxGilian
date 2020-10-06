c03f506fa63215faae4be6b906c041ebfc9f35be
LedouxGilian <56632366+LedouxGilian@users.noreply.github.com>
2020-10-06 12:26:24 +0200

    labo3

--- script/app.js
+++ script/app.js
@@ -1,13 +1,38 @@
+let email = {},
+    password = {},
+    signInButton;
+
 function handleFloatingLabel() {
    
 }
 
-function handlePasswordSwitcher() {
+const handlePasswordSwitcher = function () {
+    const passwordInput = document.querySelector(".js-password-input");
+    const passwordToggle = document.querySelector(".js-password-toggle");
+
+    if (!passwordInput || !passwordToggle) {
+        // Simplele zelfcontrole
+        console.error('De classes werden niet gevonden :/', passwordInput, passwordToggle);
+        return;
+    }
+
+    passwordToggle.addEventListener('change', function() {
+        if (passwordInput.type == 'password') {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password'
+        }
+    });
+};
+
+const getDOMElements = function() {
+    email.input = document.querySelector("");
     
-}
+};
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
-    handleFloatingLabel();
+    //handleFloatingLabel();
     handlePasswordSwitcher();
+    getDOMElements();
 });
